106bf016a3b61de5d3ec12f3200c946062fca462
Neale Pickett <neale@woozle.org>
2012-07-31 22:24:10 -0500

    google webmaster bs update

--- woozle.mk
+++ woozle.mk
@@ -1,6 +1,7 @@
 PLAIN += .
 COPY += icon.png style.css style-black.css lists.cgi wishlist.cgi set.cgi $(TEMPLATE)
 COPY += mail.cgi
+COPY += google7f698b9893809122.html
 HTML += people.html
 
 $(DESTDIR)/people.html: people.sh template.html.m4
