32f034bb8016b17cefd0e80caec19fa75d6503bb
Neale Pickett <neale@woozle.org>
2012-10-09 15:55:35 -0500

    move to roundcube

--- woozle.mk
+++ woozle.mk
@@ -1,11 +1,11 @@
 PLAIN += .
 COPY += icon.png style.css style-black.css lists.cgi wishlist.cgi set.cgi $(TEMPLATE)
-COPY += mail.cgi
+COPY += mail.cgi ilohamail.cgi
 COPY += google7f698b9893809122.html
 HTML += people.html
 
 $(DESTDIR)/people.html: people.sh template.html.m4
 	sh $< | $(MDWNTOHTML) > $@
 
-$(DESTDIR)/mail.cgi: mail.cgi.c
+$(DESTDIR)/%.cgi: %.cgi.c
 	$(CC) -Wall -Werror -o $@ $<
