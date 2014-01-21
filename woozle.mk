89b2189d46ca92355a674901cc9e9c23ff63a70a
Neale Pickett <neale@woozle.org>
2014-01-21 06:46:48 +0000

    Make front page look less like a squatter

--- woozle.mk
+++ woozle.mk
@@ -4,6 +4,7 @@ COPY += mail.cgi ilohamail.cgi l.cgi
 COPY += google7f698b9893809122.html
 HTML += people.html
 COPY += robots.txt
+COPY += derbygirl.png
 
 $(DESTDIR)/people.html: people.sh template.html.m4
 	sh $< | $(MDWNTOHTML) > $@
